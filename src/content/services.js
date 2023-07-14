0f2ff99223ba1307167d67f957d2fa3a51bd25ff
obradz <obradtf@gmail.com>
2023-07-14 17:53:35 +0200

    images added

--- src/content/services.js
+++ src/content/services.js
@@ -1,4 +1,10 @@
-import managementImage from "./assets/services/services.png";
+import managementImage from "./assets/services/management.png";
+import socialMedia from "./assets/services/social-media.jpg";
+import analytics from "./assets/services/analytics.jpg";
+import marketing from "./assets/services/marketing.jpg";
+import content from "./assets/services/content.jpg";
+
+
 
 export const servicesContent = [
     {
@@ -8,25 +14,25 @@ export const servicesContent = [
         text: 'Same one, girl with the girl at the beach "Boost your OnlyFans success with our special services for managing your profile. We\'ll help you enhance your unique style and connect with your fans, so you can grow your audience and achieve greater success!"',
     },
     {
-        img: managementImage,
+        img: content,
         subtitle: "Exos Agency",
         title: "Content",
         text: "Unleash your creativity and captivate your audience with our high-quality, innovative content creation services, designed to elevate your brand and make a lasting impression.",
     },
     {
-        img: managementImage,
+        img: socialMedia,
         subtitle: "Exos Agency",
         title: "Social Media",
         text: "Amplify your brand's voice and dominate the digital landscape with our comprehensive social media services, delivering strategic growth, engagement, and influence.",
     },
     {
-        img: managementImage,
+        img: marketing,
         subtitle: "Exos Agency",
         title: "Marketing",
         text: "Turbocharge your OnlyFans success and skyrocket your fanbase with our targeted marketing solutions, designed to elevate your presence and unlock unparalleled growth in the adult entertainment industry",
     },
     {
-        img: managementImage,
+        img: analytics,
         subtitle: "Exos Agency",
         title: "Analytics",
         text: "Harness the full power of your social media presence with our data-driven analytics, propelling your growth, fostering engagement, and unlocking unrivaled success in the digital realm",
